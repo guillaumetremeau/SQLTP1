daa91650b616c018cb0c0043854af2f38f4d7ecb
Guillaume <tremeauguillaume@gmail.com>
2017-03-24 15:20:04 +0100

    Finish tp1.sql

--- tp1.sql
+++ tp1.sql
@@ -154,32 +154,66 @@ WHERE NOT EXISTS (
 
 # Question 19
 
+SELECT NumProduit, COUNT(*) "Nbre de Livraison"
+FROM Livraisons
+GROUP BY NumProduit;
 
 
 # Question 20
 
+SELECT NumClt
+FROM Livraisons L JOIN Produits P ON L.NumProduit = P.NumProduit
+GROUP BY NumClt
+HAVING COUNT(*) = (SELECT COUNT(*) FROM Produits);
 
 
 # Question 21
 
+SELECT NumClt
+FROM Livraisons L JOIN Produits P ON L.NumProduit = P.NumProduit
+WHERE P.PrixBrut < 50
+GROUP BY NumClt
+HAVING COUNT(*) = (SELECT COUNT(*) FROM Produits WHERE PrixBrut < 50);
 
 
 # Question 22
 
+SELECT NumProduit, AVG(Quantite)
+FROM Livraisons 
+GROUP BY NumProduit
+HAVING COUNT(*) > 2
+ORDER BY NumProduit DESC;
 
 
 # Question 23
 
+SELECT P.Libelle, COUNT(*)
+FROM Livraisons L JOIN Produits P ON L.NumProduit = P.NumProduit
+GROUP BY L.NumProduit, P.libelle
+HAVING COUNT(*) = (
+	SELECT MAX(COUNT(*))
+	FROM Livraisons
+	GROUP BY NumProduit);
 
 
 # Question 24
 
+SELECT P.libelle, SUM(L.Quantite)*P.PrixBrut "chiffre d affaire"
+FROM Livraisons L JOIN Produits P ON L.NumProduit = P.NumProduit
+GROUP BY P.libelle, P.PrixBrut;
 
 
 # Question 25
 
+SELECT C.NumClt, C.NomClt, L.NumProduit, L.Quantite
+FROM Clients C LEFT JOIN Livraisons L ON C.NumClt = L.NumClt
+GROUP BY C.NumClt, C.NomClt, L.NumProduit, L.Quantite
+ORDER BY C.NumClt;
 
 
 # Question 26
 
-
+SELECT LEVEL, NumProComposant "Composant", Quantite
+FROM Composition
+CONNECT BY PRIOR NumProComposant = NumProCompose;
+START WITH NumProCompose = 40;
